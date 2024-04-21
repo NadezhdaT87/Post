b1a6986e5acd616188f92c551efc962d651825bb
NadezhdaT <kybalibri@mail.ru>
2024-04-21 23:38:41 +0700

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.name = "Иван";
+        post.passport = "4444№44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+        FormDate birthday = new FormDate();
+        birthday.day = 13;
+        birthday.month = 7;
+        birthday.year = 2010;
+        post.birthday = birthday;
+
+
+        System.out.println(post.name);
+
+    }
+
+}
